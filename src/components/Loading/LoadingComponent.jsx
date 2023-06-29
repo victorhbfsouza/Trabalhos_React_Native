001b910c8d01992bd5441428e3f944d10328f570
Gabriel Damico <gabrieldamico22@gmail.com>
2023-06-29 01:42:21 -0300

    Quase funcionando textos com delay

--- src/components/Loading/LoadingComponent.jsx
+++ src/components/Loading/LoadingComponent.jsx
@@ -1,20 +1,36 @@
-
-
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
-import LottieView from 'lottie-react-native'
-import book from '../../assets/book.json'
+import LottieView from "lottie-react-native";
+import book from "../../assets/book.json";
 
 const LoadingComponent = () => {
-
-    const [loadingText, setLoadingText] = useState(['Preparando a estante de livros...', 
-                                                    'Buscando os últimos lançamentos...', 
-                                                    'Por favor, aguarde...',
-                                                    'Estamos quase lá...' ])
+  const [loadingTexts, setLoadingTexts] = useState([
+    "Preparando a estante de livros...",
+    "Buscando os últimos lançamentos...",
+    "Por favor, aguarde...",
+    "Estamos quase lá...",
+  ]);
+  const timer = ms => new Promise(res => setTimeout(res, ms))
+  async function load () { 
+    for (var i = 0; i < loadingTexts.length; i++) {
+      console.log(loadingTexts[i]);
+      await timer(6000); 
+    }
+  }
   return (
     <View style={styles.container}>
-        <LottieView style={styles.animationLoop} source={book} loop autoPlay resizeMode='contain'/>
-        <Text style={styles.animationText}> {loadingText.map(text => {return setTimeout(() => console.log(text), 2000)})} </Text>
+      <LottieView
+        style={styles.animationLoop}
+        source={book}
+        loop
+        autoPlay
+        resizeMode="contain"
+      />
+      <Text style={styles.animationText}>
+        {   
+            load()
+        }
+      </Text>
     </View>
   );
 };
@@ -22,17 +38,17 @@ const LoadingComponent = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: '#ffffff'
+    backgroundColor: "#ffffff",
   },
-  animationLoop:{
+  animationLoop: {
     width: 250,
   },
   animationText: {
-    fontSize: 16
-  }
+    fontSize: 16,
+  },
 });
 
 export default LoadingComponent;
